1c70853cc538793b99d37c6913538a595b84d9ec
Soy <49198778+SoyWang@users.noreply.github.com>
2020-04-28 12:06:05 +0800

    Update IHKCoinService.java
    增加交易description

--- src/main/java/blockchaincode/service/IHKCoinService.java
+++ src/main/java/blockchaincode/service/IHKCoinService.java
@@ -15,23 +15,23 @@ public interface IHKCoinService {
 	 * @param path		证书文件根目录
 	 */
 	void initAccount(String username,String path) throws Exception;
-	
+
 	/**
 	 * 币操作转出 --- 奖励
 	 * @param username	被操作者
 	 * @param amount	转移数量
 	 * @param path		证书文件根目录
 	 */
-	void award(String username,String amount,String path) throws Exception;
-	
+	void award(String username,String amount,String path,String description) throws Exception;
+
 	/**
 	 * 币操作收回 --- 惩罚
 	 * @param username	被操作者
 	 * @param amount	转移数量
 	 * @param path		证书文件根目录
 	 */
-	void recycle(String username,String amount,String path) throws Exception;
-	
+	void recycle(String username,String amount,String path,String description) throws Exception;
+
 	/**
 	 * 查询用户币账户
 	 * @param username
@@ -39,14 +39,14 @@ public interface IHKCoinService {
 	 * @return
 	 */
 	Map getUserAccount(String username,String path) throws Exception;
-	
+
 	/**
 	 * 查询构件币的所有记录
 	 * @return
 	 * @throws Exception
 	 */
 	Map coinHistory(String path) throws Exception;
-	
+
 	/**
 	 * 查询指定用户构件币的操作
 	 * @param username
